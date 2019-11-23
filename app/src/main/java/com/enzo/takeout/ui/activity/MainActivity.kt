bde4b25b0d86594c120e065d7a35662db3743ed4
xiaofy <xiaofangyinwork@163.com>
2019-11-23 23:55:49 +0800

    首页添加二次推出确认

--- app/src/main/java/com/enzo/takeout/ui/activity/MainActivity.kt
+++ app/src/main/java/com/enzo/takeout/ui/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.enzo.takeout.ui.activity
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.KeyEvent
 import com.enzo.commonlib.base.BaseActivity
+import com.enzo.commonlib.utils.common.ToastUtils
+import com.enzo.commonlib.utils.statusbar.bar.StateAppBar
 import com.enzo.commonlib.widget.tablayout.TabEntityConfig
 import com.enzo.commonlib.widget.tablayout.TabLayout
 import com.enzo.commonlib.widget.tablayout.TabView
@@ -12,6 +16,7 @@ class MainActivity : BaseActivity() {
     private lateinit var tabLayout: TabLayout
 
     override fun getLayoutId(): Int {
+        StateAppBar.setStatusBarLightMode(MainActivity@ this, Color.WHITE)
         return R.layout.activity_main
     }
 
@@ -34,5 +39,21 @@ class MainActivity : BaseActivity() {
 
             }
         })
+    }
+
+    private var firstTime: Long = 0 //点击两次退出应用计时
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val secondTime = System.currentTimeMillis()
+            if (secondTime - firstTime > 2000) {
+                ToastUtils.showToast("再按一次退出程序")
+                firstTime = System.currentTimeMillis()
+                return true
+            } else {
+                System.exit(0)
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
