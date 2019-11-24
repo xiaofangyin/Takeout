d702bfe9a760e5f84b0981444d752075f3fc05ba
xiaofy <xiaofangyinwork@163.com>
2019-11-24 10:47:54 +0800

    update

---
+++ app/src/main/java/com/enzo/takeout/ui/fragment/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.enzo.takeout.ui.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.enzo.commonlib.base.BaseFragment
+
+/**
+ * 文 件 名: HomeFragment
+ * 创 建 人: xiaofy
+ * 创建日期: 2019/11/24
+ * 邮   箱: xiaofangyinwork@163.com
+ */
+class HomeFragment : BaseFragment() {
+
+    override fun getLayoutId(): Int {
+        return 0
+    }
+
+    override fun initView(rootView: View?) {
+
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun initListener(rootView: View?) {
+
+    }
+}
